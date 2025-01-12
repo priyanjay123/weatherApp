fbe55e34342b29aa8c94be99e88ceb2c5d607350
Priyanjay Goswami <priyanjay.goswami150193@gmailc.com>
2025-01-12 13:52:46 +0530

    weatherApp

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Weather from "./components/Weather";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,97 @@
+import react, { useEffect, useRef, useState } from "react";
+import "./Weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import humidity_icon from "../assets/humidity.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+
+const Weather = () => {
+  const inputRef = useRef();
+  const [weatherData, setWeatherData] = useState(false);
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      search(inputRef.current.value);
+    }
+  };
+
+  const search = async (city) => {
+    if (city === "") {
+      alert("Enter City Name");
+      return;
+    }
+
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log("JSON Data:", data); // Logging the JSON data
+      //const icon  = allIcons[data.we[0].icon]
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temparature: Math.floor(data.main.temp),
+        location: data.name,
+      });
+    } catch (error) {
+      setWeatherData(false);
+    }
+  };
+
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Search"
+          onKeyDown={handleKeyDown}
+        />
+        <img
+          src={search_icon}
+          alt=""
+          onClick={() => search(inputRef.current.value)}
+        />
+      </div>
+
+      {weatherData ? (
+        <>
+          <img src={clear_icon} className="weather-icon" alt="" />
+          <p className="temparature">{weatherData.temparature}°c </p>
+          <p className="location">{weatherData.location} </p>
+          <div className="weather-data">
+            <div className="col">
+              <img src={humidity_icon} alt="" />
+              <div>
+                <p>{weatherData.humidity}</p>
+                <span>Humidity</span>
+              </div>
+            </div>
+            <div className="col">
+              <img src={wind_icon} alt="" />
+              <div>
+                <p>{weatherData.windSpeed} km/h</p>
+                <span>Wind Speed</span>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
